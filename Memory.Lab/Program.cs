db7f84569d0425e4378fc2597515545be0f506df
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-08 00:36:46 +0330

    Fix

---
+++ Memory.Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Memory.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 5;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Structs.Lab/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Structs.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class CoordRef
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public CoordRef(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    struct CoordVal
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public CoordVal(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+}
